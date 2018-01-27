6dbbcafa6a623cbecb270dfbf8e91cc34c64b5c7
IronLu233 <lrironsora@gmail.com>
2018-01-28 02:31:39 +0800

    add windows scripts

---
+++ start-container.ps1
@@ -0,0 +1,31 @@
+$VOLUME = $HOME + "/src/"
+$SLAVE_COUNT = 2
+
+if (-not (Test-Path $VOLUME))
+{
+	mkdir $VOLUME
+}
+Write-Output "start hadoop-master container..."
+{
+	docker run -itd `
+	--net=hadoop `
+	-p 50070:50070 `
+	-p 8088:8088 `
+	-v ~/src:/root/src `
+	--name hadoop-master `
+	--hostname hadoop-master `
+	joway/hadoop-cluster
+} | Out-Null
+
+for ($i = 0; $i -lt $SLAVE_COUNT; $i++) {
+    docker rm -f hadoop-slave$i  | Out-Null
+	Write-Output "start hadoop-slave$i container..."
+	{
+		docker run -itd `
+	    --net=hadoop `
+		--name hadoop-slave$i `
+		--hostname hadoop-slave$i `
+		joway/hadoop-cluster 
+	} | Out-Null
+}
+docker exec -it hadoop-master bash
